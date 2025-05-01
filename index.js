d082a52b26a397cb1ead891b52a767952ed2874c
DevendraBhide <dbhide7@gmail.com>
2025-05-01 17:16:49 +0530

    first commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static('public/'))
  var adminRoute=require("./routes/adminroute")
  app.use('/admin',adminRoute)
  
-const port=3001  || process.env.port;
+const port=process.env.port || 3001;
 app.listen(port,()=>{
     console.log(`server run at http://localhost:${port}`)
 })
